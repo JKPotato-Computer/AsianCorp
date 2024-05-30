e3986469217dd6ce42ffdaf52b569dd9ff14f882
JKPotato-Computer <jkpotato.compurer@gmail.com>
2024-05-29 22:52:56 -0500

    yeaaah

--- js/index.js
+++ js/index.js
@@ -17,16 +17,18 @@ for (const element of document.querySelectorAll('main div.galleryDisplay, main >
 	observer.observe(element);
 }
 
-document.querySelector("#preOrder").onclick = function() {
-	n += 1;
-	document.querySelectorAll(".bagCount")[0].textContent = n;
-
-	if (n >= 100) {
-		document.querySelectorAll("a:has(.bagCount)")[0].style.backgroundColor = "rgb(255 0 0)";
-	}
-}
-
 document.querySelector("#ThemeChanger").onclick = function() {
 	document.documentElement.dataset.theme = (document.documentElement.dataset.theme == "light") ? ("dark") : ("light");
 	this.textContent = ((document.documentElement.dataset.theme == "light") ? ("Dark") : ("Light")) + " Theme"
+}
+
+if (document.querySelector("#preOrder")) {
+	document.querySelector("#preOrder").onclick = function() {
+		n += 1;
+		document.querySelectorAll(".bagCount")[0].textContent = n;
+	
+		if (n >= 100) {
+			document.querySelectorAll("a:has(.bagCount)")[0].style.backgroundColor = "rgb(255 0 0)";
+		}
+	}
 }
